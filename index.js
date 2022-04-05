47038d66bedc86607ba6ccebdb95e33c37220b0b
vinaym <vinaym@maveric-systems.com>
2022-04-05 13:42:54 +0530

    edit and delete option

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ var List = Backbone.Model.extend({
     }
 });
 
+var id_val = Backbone.Model.extend({
+    defaults: {
+        id: ''
+    }
+});
+
+var idValue = new id_val();
+
 var Collection_list = Backbone.Collection.extend({
 
 });
@@ -29,22 +37,40 @@ var ListView = Backbone.View.extend({
         return this;
     },
     events: {
-        'click .like-btn': 'like'
+        'click .like-btn': 'like',
+        'click .edit-btn': 'edit',
+        'click .delete-btn': 'delete',
     },
-    like: function (e) {
+    like: function () {
         this.model.set({
             likes: this.model.get('likes') + 1
         });
-
+    },
+    edit: function () {
+        console.log('edit', this.model, collection_list, idValue);
+        $('.name-input').val(this.model.get('name'));
+        $('.age-input').val(this.model.get('age')),
+            $('.mobile-input').val(this.model.get('mobile')),
+            $('.email-input').val(this.model.get('email'));
+        $('.add-list').hide();
+        $('.update-list').show();
+        idValue.set('id', this.model.cid);
+    },
+    delete: function () {
+        console.log('delete', this.model);
+        this.model.destroy();
     }
-
 });
 
 var viewLists = Backbone.View.extend({
     model: collection_list,
     el: $('.blogs-list'),
     initialize: function () {
+        // $('.update-list').hide();
         this.model.on('add', this.render, this);
+        this.model.on('change', this.render, this);
+        this.model.on('remove', this.render, this);
+
     },
 
     render: function () {
@@ -57,21 +83,7 @@ var viewLists = Backbone.View.extend({
     }
 });
 
-// var viewBlock = Backbone.View.extend({
-//     el: $('.view-block'),
-//     render: function () {
-//         this.$el.html('display');
-//         // // _.each(this.model.toArray(), function (List) {
-//         //     self.$el.append((new ListView({ model: List })).render().$el);
-//         // // });
-//         // return this;
-//     }
-// });
-
 var ViewLists = new viewLists();
-
-// var ViewBlock = new viewBlock();
-// ViewBlock.render();
 
 $(document).ready(function () {
     $('.add-list').on('click', function () {
@@ -86,5 +98,21 @@ $(document).ready(function () {
         $('.mobile-input').val('');
         $('.email-input').val('');
         collection_list.add(list);
+    });
+    $('.update-list').on('click', function () {
+        collection_list.map(function(element, i) {
+            if(idValue.get('id') === element.cid){
+                collection_list.models[i].set('name', $('.name-input').val());
+                collection_list.models[i].set('age', $('.age-input').val());
+                collection_list.models[i].set('mobile', $('.mobile-input').val());
+                collection_list.models[i].set('email', $('.email-input').val()); 
+            }
+        })
+        $('.name-input').val('');
+        $('.age-input').val('');
+        $('.mobile-input').val('');
+        $('.email-input').val('');
+        $('.add-list').show();
+        $('.update-list').hide();
     });
 })
